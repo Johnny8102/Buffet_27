49b302eef78782c8e8fd894dd43a8e866ff7fc3b
Johnny8102 <johnnyghevondyan2011@gmail.com>
2026-09-09 18:02:05 +0000

    update

--- Java/000_Labs/003_output/base_code/starter.java
+++ Java/000_Labs/003_output/base_code/starter.java
@@ -1,6 +1,6 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Johnny Ghevondyan
+ *  Date: 9/2/26
 */
 
 import java.util.Scanner;
@@ -9,6 +9,12 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+
+		System.out.println("Happy Birthday, to you.");
+		System.out.println("Happy Birthday, to you.");
+		System.out.println("Happy Birthday dear Johnny,");
+		System.out.println("Happy Birthday, to you.");
+
+
 	}
 }
--- Java/000_Labs/004_String_class/base_code/starter.java
+++ Java/000_Labs/004_String_class/base_code/starter.java
@@ -9,6 +9,11 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String a = new String ("Happy Birthday, to you."); 
+		String b = new String ("Happy birthday dear Johnny,");
+		System.out.println(a);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.print(a);
 	}
 }
