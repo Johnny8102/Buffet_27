d18cbae96fb0d33226c51039ffdc8ebbdb653186
Johnny8102 <johnnyghevondyan2011@gmail.com>
2026-09-14 19:00:34 +0000

    update

--- Java/000_Labs/008_input/base_code/starter.java
+++ Java/000_Labs/008_input/base_code/starter.java
@@ -1,12 +1,31 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Johnny Ghevondyan
+ *  Date: 9/9/26
 */
 
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = sc.nextLine ();
+		System.out.println("What is your age? ");
+		int age = sc.nextInt();
+		System.out.println("what month were you born? (1-12)");
+		int month = sc.nextInt();
+		System.out.println ("What day were you born (1-31)");
+		int day = sc.nextInt();
+		System.out.println ("What year were you born");
+		int year = sc.nextInt();
+
+
+
+
+
+
+
+
+
 	}
 }
