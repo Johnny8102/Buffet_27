7b60b3e7c8f1cec1d1fd265b0ed0831a186e3be0
Johnny8102 <johnnyghevondyan2011@gmail.com>
2026-09-15 18:03:57 +0000

    update

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,27 @@
 /*
     Lecture note example - Input!!
 */
-
+import java.util.Scanner;
 class LectureInput{
     public static void main(String args[]) {
-        // Your Code Goes here!
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Johnnys in-n-out, Heres our menu.");
+        double b = (5.50);
+        double f = (2.50);
+        double s = (2);
+        System.out.println("Burgers: " + b + "$");
+        System.out.println("Fries: " + f + "$");
+        System.out.println("Soda: " + s + "$");
+        System.out.println("How many burgers would you like?");
+        int burgerquant = sc.nextInt();
+        System.out.println("How many fries would you like?");
+        int friesquant = sc.nextInt();
+        System.out.println("How many sodas would you like?");
+        int sodaquant = sc.nextInt();
+        System.out.println ("Here is your receipt");
+        double burgertotal = (burgerquant*b);
+
+
+        
 	}
 }
