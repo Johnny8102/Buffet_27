cf65e1650144384c153f32b6f1ae312b104aae3e
Johnny8102 <johnnyghevondyan2011@gmail.com>
2026-09-21 18:51:15 +0000

    update

--- Java/000_Labs/010_random_method-static/base_code/starter.java
+++ Java/000_Labs/010_random_method-static/base_code/starter.java
@@ -12,8 +12,10 @@ class starter {
 		System.out.println(x);
 		int y = (int)(Math.random()*100)+0;
 		System.out.println(y);
-		double z = (Math.random()*3.5+2.5);
+		double z = (double) (Math.random()*1)+2.5;
 		System.out.println(z);
+		double a = (double) (Math.random()*575)+14;
+		System.out.println(a);
 		
 
 		
--- Java/102_Exercises/10_RandomRange/base_code/starter.java
+++ Java/102_Exercises/10_RandomRange/base_code/starter.java
@@ -8,8 +8,28 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		System.out.println ("Please enter 2 numbers to create a range for your random numbers");
+		System.out.println ("please enter a integer:");
+		int num1 = sc.nextInt();
+		System.out.println("please enter another intger bigger than the first:");
+		int num2 = sc.nextInt();
+		System.out.println("Your range is " + num1 + "-" + num2);
+		System.out.println(" Here are 5 numbers generated in that range.");
+		int x = (int) (Math.random()*num2-num1)+num1;
+		int y = (int) (Math.random()*num2-num1)+num1;
+		int z = (int) (Math.random()*num2-num1)+num1;
+		int a = (int) (Math.random()*num2-num1)+num1;
+		int b = (int) (Math.random()*num2-num1)+num1;
+		System.out.print(x + ", ");
+		System.out.print(y + ", ");
+		System.out.print(z + ", ");
+		System.out.print(a + ", ");
+		System.out.print(b);
+		
+
+		
+		
+
 	}
 }
